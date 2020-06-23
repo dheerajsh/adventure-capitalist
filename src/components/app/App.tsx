956e7699950faca4d478ee0cf8ecdc6563e93bc6
dheeraj <dheeraj@ddproperty.com>
2020-06-23 20:06:35 +0700

    Moved data out

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -4,7 +4,6 @@ import './App.css';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import { BusinessState } from '../business/business';
 import Business from '../../containers/business';
 import Manager from '../../containers/manager';
 import Upgrader from '../../containers/upgrader';
@@ -17,7 +16,9 @@ import { StoreState } from '../../store/StoreState'
 import { Map } from 'immutable';
 import store from '../../store/Store';
 import { Provider } from 'react-redux';
-
+import businesses from '../../resources/businesses.json'
+import managers from '../../resources/managers.json'
+import upgraders from '../../resources/upgraders.json'
 export interface IAppProps {
   readonly userMoney?: number,
   readonly lastActiveTime?: number,
@@ -66,47 +67,21 @@ export class App extends React.Component<IAppProps, IAppState> {
   }
 
   getBusinesses(): JSX.Element {
-    return (<>
-      <Business
-        name='Lemonade Stand'
-        value={2}
-        state={BusinessState.PURCHASED}
+    const businessess = businesses.map((business) => {
+      return (<>
+        <Business
+        name={business.name}
+        value={business.value}
         numberOfBusiness={1}
         overallValue={this.state.userMoney}
-        makeMoneyTimeInMiliSeconds={2000}
-      />
-      <hr />
-      <Business
-        name='News Delivery'
-        value={60}
-        numberOfBusiness={0}
-        overallValue={this.state.userMoney}
-        makeMoneyTimeInMiliSeconds={3000}
-      />
-      <hr />
-      <Business
-        name='Car Wash'
-        value={7200}
-        numberOfBusiness={0}
-        overallValue={this.state.userMoney}
-        makeMoneyTimeInMiliSeconds={5000}
-      />
-      <hr />
-      <Business
-        name='Pizza Delivery'
-        value={58640}
-        numberOfBusiness={0}
-        overallValue={this.state.userMoney}
-        makeMoneyTimeInMiliSeconds={8000}
-      />
-      <hr />
-      <Business
-        name='Donut Shop'
-        value={103680}
-        numberOfBusiness={0}
-        overallValue={this.state.userMoney}
-        makeMoneyTimeInMiliSeconds={16000}
-      />
+        makeMoneyTimeInMiliSeconds={business.makeMoneyTimeInMiliSeconds}
+        />
+        <hr/>
+      </>)
+
+    })
+    return (<>
+      {businessess}
     </>);
   }
 
@@ -121,40 +96,19 @@ export class App extends React.Component<IAppProps, IAppState> {
           </Card.Header>
           <Accordion.Collapse eventKey="0">
             <Card.Body>
-              <Manager
-                businessName='Lemonade Stand'
-                value={2000}
-                name='Marcus'
-                overallValue={this.state.userMoney}
-              />
-              <hr />
-              <Manager
-                businessName='News Delivery'
-                value={10000}
-                name='The undertaker'
-                overallValue={this.state.userMoney}
-              />
-              <hr />
-              <Manager
-                businessName='Car Wash'
-                value={105000}
-                name='The Rock'
-                overallValue={this.state.userMoney}
-              />
-              <hr />
-              <Manager
-                businessName='Donut Shop'
-                value={500500}
-                name='Stephani'
-                overallValue={this.state.userMoney}
-              />
-              <hr />
-              <Manager
-                businessName='Pizza Delivery'
-                value={1005000}
-                name='The Dark lord'
-                overallValue={this.state.userMoney}
-              />
+              {
+                managers.map((manager) => {
+                  return (<>
+                   <Manager
+                      businessName={manager.businessName}
+                      value={manager.value}
+                      name={manager.name}
+                      overallValue={this.state.userMoney}
+                      />
+                  <hr />
+                  </>)
+                })
+              }
             </Card.Body>
           </Accordion.Collapse>
         </Card>
@@ -173,40 +127,19 @@ export class App extends React.Component<IAppProps, IAppState> {
           </Card.Header>
           <Accordion.Collapse eventKey="0">
             <Card.Body>
-              <Upgrader
-                businessName='Lemonade Stand'
-                value={13000}
-                times={3}
-                overallValue={this.state.userMoney}
-              />
-              <hr />
-              <Upgrader
-                businessName='News Delivery'
-                value={145000}
-                times={3}
-                overallValue={this.state.userMoney}
-              />
-              <hr />
-              <Upgrader
-                businessName='Car Wash'
-                value={305000}
-                times={3}
-                overallValue={this.state.userMoney}
-              />
-              <hr />
-              <Upgrader
-                businessName='Donut Shop'
-                value={607000}
-                times={2}
-                overallValue={this.state.userMoney}
-              />
-              <hr />
-              <Upgrader
-                businessName='Pizza Delivery'
-                value={1005000}
-                times={2}
-                overallValue={this.state.userMoney}
-              />
+            {
+                upgraders.map((upgrader) => {
+                  return (<>
+                   <Upgrader
+                      businessName={upgrader.businessName}
+                      value={upgrader.value}
+                      times={upgrader.times}
+                      overallValue={this.state.userMoney}
+                      />
+                  <hr />
+                  </>)
+                })
+              }
             </Card.Body>
           </Accordion.Collapse>
         </Card>
